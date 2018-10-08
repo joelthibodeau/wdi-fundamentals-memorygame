eca90058edd8028f81ce5fb6ea759e3d5b3f3d08
Spake <joel.thibodeau@gmail.com>
2018-10-08 05:39:15 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,19 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0] // Stores the FIRST item in the cards array in the variable cardOne.
-cardsInPlay.push(cardOne) // Adds cardOne to the end of the cardsInPlay array.
-var cardTwo = cards[1] // Stores the THIRD item in the cards array in the variable cardTwo.
-cardsInPlay.push(cardTwo)
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
 		alert("You found a match!"); 
-  } else {
+  	} 	else {
 		alert("Sorry, try again.");
-  }	 // Checks to see if the legth of the cardsInPlay array is 2.
+  	}
 }
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+  	}	
+}
+flipCard(0);
+flipCard(2);
