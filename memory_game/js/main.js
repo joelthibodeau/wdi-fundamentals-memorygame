c768c4cf774520f7dfd1a1cea8e1cce30f561109
joelthibodeau <joel.thibodeau@gmail.com>
2018-10-11 21:49:38 -0400

    created game logic - tried button & counter

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+// card face rank, suit and image
 var cards = [
 {
 rank: "queen",
@@ -20,14 +21,17 @@ suit: "diamonds",
 cardImage: "images/king-of-diamonds.png",
 }
 ];
+// cards that are in play once clicked
 var cardsInPlay = [];
+
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!"); 
   	} 	else {
 		alert("Sorry, try again.");
   	}
 }
+// flips card to show face when clicked
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
@@ -39,15 +43,30 @@ var flipCard = function() {
 	checkForMatch();
   	}	
 }
-
-var createBoard = function () {
+// displays all four back faces of cards
+ var createBoard = function () {
 	for (var i = 0; i < cards.length; i++) {
 	var cardElement = document.createElement('img');
 	cardElement.setAttribute('src', 'images/back.png');
 	cardElement.setAttribute('data-id', i);
 	cardElement.addEventListener('click', flipCard);
 	document.getElementById('game-board').appendChild(cardElement);
+    document.getElementsByTagName('div').innerHTML = null; // new code for reset button 
+	}
 }
+/*var resetGame = function () {
+	document.getElementsById('reset-button'); {
+	cardElement.addEventListener('click', createBoard);
+    document.getElementsByTagName('div').innerHTML = null;
+
+	 document.getElementsByTagName('div').innerHTML = null; */
+	/* addEventListener('click', createBoard); */
+/* 
+
+	var cardsInPlay = []; 
 }
+} */
 createBoard();
+
+
 
